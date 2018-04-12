07c79cce6afaf8bab50106539b1ad3bef660cbae
EVGA-ПК\EVGA <largow10@gmail.com>
2018-04-12 15:22:42 +0300

    add string

--- app/src/main/java/com/gkain/weather/altarixweather/MainActivity.java
+++ app/src/main/java/com/gkain/weather/altarixweather/MainActivity.java
@@ -2,6 +2,9 @@ package com.gkain.weather.altarixweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView cityname = (TextView) findViewById(R.id.cityname);
+
+
+
     }
+
 }
